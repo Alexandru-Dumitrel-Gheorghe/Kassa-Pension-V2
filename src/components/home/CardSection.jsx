4fc80572d8c6665bf36a2e1e70b9c70d61a0ee83
Alexandru-Dumitrel-Gheorghe <167351297+Alexandru-Dumitrel-Gheorghe@users.noreply.github.com>
2024-08-03 22:57:46 +0200

    Some Updates

--- src/components/home/CardSection.jsx
+++ src/components/home/CardSection.jsx
@@ -14,62 +14,54 @@ import { useTranslation } from "react-i18next";
 const CardSection = () => {
   const { t } = useTranslation();
 
+  const facilities = [
+    { icon: faWifi, text: t("free_wifi") },
+    { icon: faTv, text: t("tv") },
+    { icon: faParking, text: t("free_parking") },
+    { icon: faUtensils, text: t("barbecue") },
+    { icon: faCoffee, text: t("cafe") },
+  ];
+
+  const cards = [
+    {
+      imgSrc:
+        "https://cf.bstatic.com/xdata/images/hotel/max1024x768/553229625.jpg?k=0b9ebe916f419bce7c7c2ce2e8f910091cd5a3a38c0171ba269d5098e8edd15b&o=&hp=1",
+      title: t("premium_services"),
+      hoverText: t("enjoy_best_services"),
+    },
+    {
+      imgSrc:
+        "https://cf.bstatic.com/xdata/images/hotel/max1024x768/553229569.jpg?k=1d6feb261ec3218271b48c63dd2104ecfc0d45d75f9c6cf54a2193ea7518ff99&o=&hp=1",
+      title: t("modern_facilities"),
+      hoverText: t("rooms_equipped_with_modern_facilities"),
+    },
+    {
+      imgSrc:
+        "https://cf.bstatic.com/xdata/images/hotel/max1024x768/554933423.jpg?k=a114d1eb74df051761c655bd12fda96c8bf62f79ba1a1ff339535d473cd64833&o=&hp=1",
+      title: t("relaxing_atmosphere"),
+      hoverText: t("quiet_place_to_relax"),
+    },
+  ];
+
   return (
     <Container className="kassa-card-section mt-5">
       <h2 className="section-title">{t("facilities")}</h2>
       <div className="facilities-list">
-        <div className="facility-item">
-          <FontAwesomeIcon icon={faWifi} className="facility-icon" />
-          {t("free_wifi")}
-        </div>
-        <div className="facility-item">
-          <FontAwesomeIcon icon={faTv} className="facility-icon" />
-          {t("tv")}
-        </div>
-        <div className="facility-item">
-          <FontAwesomeIcon icon={faParking} className="facility-icon" />
-          {t("free_parking")}
-        </div>
-        <div className="facility-item">
-          <FontAwesomeIcon icon={faUtensils} className="facility-icon" />
-          {t("barbecue")}
-        </div>
-        <div className="facility-item">
-          <FontAwesomeIcon icon={faCoffee} className="facility-icon" />
-          {t("cafe")}
-        </div>
+        {facilities.map((facility, index) => (
+          <div key={index} className="facility-item">
+            <FontAwesomeIcon icon={facility.icon} className="facility-icon" />
+            {facility.text}
+          </div>
+        ))}
       </div>
       <div className="kassa-cards-container">
-        <div className="kassa-card">
-          <img
-            src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/553229625.jpg?k=0b9ebe916f419bce7c7c2ce2e8f910091cd5a3a38c0171ba269d5098e8edd15b&o=&hp=1"
-            alt={t("premium_services")}
-          />
-          <div className="kassa-card-title">{t("premium_services")}</div>
-          <div className="kassa-card-hover-text">
-            {t("enjoy_best_services")}
+        {cards.map((card, index) => (
+          <div key={index} className="kassa-card">
+            <img src={card.imgSrc} alt={card.title} />
+            <div className="kassa-card-title">{card.title}</div>
+            <div className="kassa-card-hover-text">{card.hoverText}</div>
           </div>
-        </div>
-        <div className="kassa-card">
-          <img
-            src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/553229569.jpg?k=1d6feb261ec3218271b48c63dd2104ecfc0d45d75f9c6cf54a2193ea7518ff99&o=&hp=1"
-            alt={t("modern_facilities")}
-          />
-          <div className="kassa-card-title">{t("modern_facilities")}</div>
-          <div className="kassa-card-hover-text">
-            {t("rooms_equipped_with_modern_facilities")}
-          </div>
-        </div>
-        <div className="kassa-card">
-          <img
-            src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/554933423.jpg?k=a114d1eb74df051761c655bd12fda96c8bf62f79ba1a1ff339535d473cd64833&o=&hp=1"
-            alt={t("relaxing_atmosphere")}
-          />
-          <div className="kassa-card-title">{t("relaxing_atmosphere")}</div>
-          <div className="kassa-card-hover-text">
-            {t("quiet_place_to_relax")}
-          </div>
-        </div>
+        ))}
       </div>
     </Container>
   );
